1c0373e24a0f1ba0928e161c3a0311eb734c669f
lyy <qcliuyy>
2024-10-01 17:23:39 +0800

    no message

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
 @echo on
 ::cargo clean
-cargo build --release
+::rustup target add x86_64-unknown-linux-gnu
+cargo build --release --target=x86_64-pc-windows-msvc
+cargo build --release --target=x86_64-pc-windows-gnu
+wsl proxychains /root/.cargo/bin/cargo build --release --target=x86_64-unknown-linux-gnu
+wsl proxychains /root/.cargo/bin/cargo build --release --target=aarch64-unknown-linux-gnu
+::wsl proxychains /root/.cargo/bin/cargo build --release --target=x86_64-linux-android
+::wsl proxychains /root/.cargo/bin/cargo build --release --target=aarch64-linux-android
